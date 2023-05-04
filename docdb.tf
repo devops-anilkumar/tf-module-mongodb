3605ae172ae180c4497caacd514fe0c4711f15b4
unknown <anilkumarravuri510@gmail.com>
2023-05-05 01:46:41 +0530

    auto push

--- docdb.tf
+++ docdb.tf
@@ -1,19 +1,23 @@
-resource "aws_docdb_cluster" "docdb" {
-  cluster_identifier = "robot-${var.ENV}-docdb"
-  engine                  = "docdb"
-  master_username         = "admin1"
-  master_password         = "roboshop1"
-  //backup_retention_period = 5
-  //preferred_backup_window = "07:00-09:00"
-  skip_final_snapshot     = true
-}
+# resource "aws_docdb_cluster" "docdb" {
+#   cluster_identifier = "robot-${var.ENV}-docdb"
+#   engine                  = "docdb"
+#   master_username         = "admin1"
+#   master_password         = "roboshop1"
+#   //backup_retention_period = 5
+#   //preferred_backup_window = "07:00-09:00"
+#   skip_final_snapshot     = true
+# }
 
 # CREATES SUBNET GROUP NEEDED TO HOST THE DOCDB CLUSTER
 resource "aws_docdb_subnet_group" "docdb_subnet-group" {
   name       = "robot-${var.ENV}-docdb-subnet-group"
-  subnet_ids = [aws_subnet.frontend.id, aws_subnet.backend.id]
+  subnet_ids = data.terraform_remote_state.vpc.PRIVATE_SUBNET_IDS
 
   tags = {
-    Name = "My docdb subnet group"
+    Name = "robot-${var.ENV}-docdb-subnet-group"
   }
+}
+
+output "output_ref" {
+    value = data.terraform_remote_state.vpc
 }
