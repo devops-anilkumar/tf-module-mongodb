9ef0ec703874fed606479a2666ee3a7c4c13af0b
unknown <anilkumarravuri510@gmail.com>
2023-05-05 01:55:55 +0530

    auto push

--- docdb.tf
+++ docdb.tf
@@ -1,12 +1,14 @@
-# resource "aws_docdb_cluster" "docdb" {
-#   cluster_identifier = "robot-${var.ENV}-docdb"
-#   engine                  = "docdb"
-#   master_username         = "admin1"
-#   master_password         = "roboshop1"
-#   //backup_retention_period = 5
-#   //preferred_backup_window = "07:00-09:00"
-#   skip_final_snapshot     = true
-# }
+# CREATING A CLUSTER OF DOCUMENT DB
+resource "aws_docdb_cluster" "docdb" {
+  cluster_identifier = "robot-${var.ENV}-docdb"
+  engine                  = "docdb"
+  master_username         = "admin1"
+  master_password         = "roboshop1"
+  //backup_retention_period = 5
+  //preferred_backup_window = "07:00-09:00"
+  skip_final_snapshot     = true
+  db_subnet_group_name    = aws_docdb_subnet_group.docdb_subnet-group.name
+}
 
 # CREATES SUBNET GROUP NEEDED TO HOST THE DOCDB CLUSTER
 resource "aws_docdb_subnet_group" "docdb_subnet-group" {
