08c73bf1623a6606069c10a327d0a3f9a92b15f4
unknown <anilkumarravuri510@gmail.com>
2023-05-05 02:29:24 +0530

    auto push

--- docdb.tf
+++ docdb.tf
@@ -8,6 +8,7 @@ resource "aws_docdb_cluster" "docdb" {
   //preferred_backup_window = "07:00-09:00"
   skip_final_snapshot     = true
   db_subnet_group_name    = aws_docdb_subnet_group.docdb_subnet-group.name
+  vpc_security_group_ids  =[aws_security_group.allow_mongodb.id]
 }
 
 # CREATES SUBNET GROUP NEEDED TO HOST THE DOCDB CLUSTER
