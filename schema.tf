36159f8356519c11327735e1b5835d0532fa6015
unknown <anilkumarravuri510@gmail.com>
2023-05-06 14:49:23 +0530

    auto push

--- schema.tf
+++ schema.tf
@@ -2,9 +2,9 @@
 resource "null_resource" "schema" {
   provisioner "local-exec" {
   command = <<EOF
-       
+       cd /tmp
        curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
-       unzip -o mongodb.zip
+       unzip /tmp/mongodb.zip
        cd mongodb-main
        wget https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem
        mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} robot-dev-docdb-instance.c3j2gupablhn.us-east-1.docdb.amazonaws.com:27017 --sslCAFile rds-combined-ca-bundle.pem --username admin1 --password roboshop1 < users.js
