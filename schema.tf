54126df6997adfa89e755517c7b3786accf11f0e
unknown <anilkumarravuri510@gmail.com>
2023-05-06 14:36:41 +0530

    auto push

--- schema.tf
+++ schema.tf
@@ -4,7 +4,7 @@ resource "null_resource" "schema" {
   command = <<EOF
        cd /tmp
        curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
-       unzip /tmp/mongodb.zip
+       unzip -o mongodb.zip
        cd mongodb-main
        wget https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem
        mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} robot-dev-docdb-instance.c3j2gupablhn.us-east-1.docdb.amazonaws.com:27017 --sslCAFile rds-combined-ca-bundle.pem --username admin1 --password roboshop1 <  users.js
