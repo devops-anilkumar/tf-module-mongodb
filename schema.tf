65e4c378f40a6240460c8b27f9281579406a3f51
unknown <anilkumarravuri510@gmail.com>
2023-05-06 14:35:05 +0530

    auto push

--- schema.tf
+++ schema.tf
@@ -9,7 +9,6 @@ resource "null_resource" "schema" {
        wget https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem
        mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} robot-dev-docdb-instance.c3j2gupablhn.us-east-1.docdb.amazonaws.com:27017 --sslCAFile rds-combined-ca-bundle.pem --username admin1 --password roboshop1 <  users.js
        mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} robot-dev-docdb-instance.c3j2gupablhn.us-east-1.docdb.amazonaws.com:27017 --sslCAFile rds-combined-ca-bundle.pem --username admin1 --password roboshop1 < catalogue.js
-       mysql -h  ${aws_db_instance.mysql.address}  -uadmin1  -pRoboShop1 < shipping.sql
 
     EOF
   }
