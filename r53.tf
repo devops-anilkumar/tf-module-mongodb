b7aaec10047e405b027eafdec5dd99ff2a8961ed
unknown <anilkumarravuri510@gmail.com>
2023-05-06 17:13:32 +0530

    auto push

--- r53.tf
+++ r53.tf
@@ -1,8 +1,8 @@
-# THIS CREATES THE C NAME RECORD FOR MONGODB
-resource "aws_route53_record" "mongodb" {
-  zone_id = "Z01019823KGEEYKYEE9GQ"
-  name    = "www.example.com"
-  type    = "A"
-  ttl     = 300
-  records = [aws_eip.lb.public_ip]
-}
+# # THIS CREATES THE C NAME RECORD FOR MONGODB
+# resource "aws_route53_record" "mongodb" {
+#   zone_id = "Z01019823KGEEYKYEE9GQ"
+#   name    = "www.example.com"
+#   type    = "A"
+#   ttl     = 300
+#   records = [aws_eip.lb.public_ip]
+# }
