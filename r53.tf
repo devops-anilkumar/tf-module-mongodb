7275f32a2506f9f952dabbcbd1cb4015f60a6647
unknown <anilkumarravuri510@gmail.com>
2023-05-08 11:33:10 +0530

    auto push

--- r53.tf
+++ r53.tf
@@ -1,11 +1,11 @@
 # THIS CREATES THE C NAME RECORD FOR MONGODB
-# resource "aws_route53_record" "mongodb" {
-#   zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID
-#   name    = "mongodb-${var.ENV}"
-#   type    = "CNAME"
-#   ttl     = 10
-#   records = [aws_docdb_cluster.docdb.endpoint]
-# }
+resource "aws_route53_record" "mongodb" {
+  zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID
+  name    = "mongodb-${var.ENV}"
+  type    = "CNAME"
+  ttl     = 10
+  records = [aws_docdb_cluster.docdb.endpoint]
+}
 
 # A Record      :  creating Name to an IP Address
 # C NAME Record :  creating name to a Name
