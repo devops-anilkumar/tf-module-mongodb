a40aa9872ba876214adbced2864783b5cbaba91e
unknown <anilkumarravuri510@gmail.com>
2023-05-17 00:27:15 +0530

    auto push

--- schema.tf
+++ schema.tf
@@ -4,7 +4,6 @@ resource "null_resource" "schema" {
   provisioner "local-exec" {
   command = <<EOF
        cd /tmp
-       rm -rf /tmp/mongodb-main
        curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
        unzip -o mongodb.zip
        cd mongodb-main
